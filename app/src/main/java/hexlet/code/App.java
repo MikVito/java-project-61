6e2220f19270e2a5a17d65442e18fd7426f131f1
MikVit <mikhailvitoshnev@hotmail.com>
2024-08-18 23:28:41 +0300

    Was imported Scanner class to App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,16 @@
 package hexlet.code;
-
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Welcome to the Brain Games!");
+        System.out.print("What is your name?\n");
+        String userName = scanner.nextLine();
+
+        System.out.println("Hello, " + userName + "!");
+        scanner.close();
     }
 }
+
+
