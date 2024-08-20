84b7f7e4aafaec8d5a858820e305d51e9a825119
MikVit <mikhailvitoshnev@hotmail.com>
2024-08-20 22:46:24 +0300

    added getName() to the Cli class

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,14 +3,20 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void cli() {
+    private String userName;
+
+    public void cli() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the Brain Games!");
         System.out.print("What is your name?\n");
-        String userName = scanner.nextLine();
+        userName = scanner.nextLine();
 
         System.out.println("Hello, " + userName + "!");
         scanner.close();
+    }
+
+    public String getUserName() {
+        return userName;
     }
 }
